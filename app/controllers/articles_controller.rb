e16073093c6d14eb05c641e68513edba88b12b31
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-02-16 23:26:32 +0100

    Add the create form

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,12 +9,29 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   def create
-    @article = Article.new(title: "...", body: "...")
+    @article = Article.new(articles_params)
 
     if @article.save
       redirect_to @article
     else
       render :new
     end
+  end
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render :edit
+    end
+  end
+  private
+  def articles_params
+    params.require(:article).permit(:title, :body)
   end
 end
