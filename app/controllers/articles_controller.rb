3a9b89575ce1788ca26321d4037e0365c4c31b3f
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-02-17 12:34:21 +0100

    Add the destroy feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -30,6 +30,12 @@ class ArticlesController < ApplicationController
       render :edit
     end
   end
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to root_path
+  end
   private
   def articles_params
     params.require(:article).permit(:title, :body)
